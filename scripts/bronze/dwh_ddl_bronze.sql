db09eaed764a77ddb15baeab615fdfc90f9d6ab3
Rajnish <rajnishjangu785@gmail.com>
2026-05-29 12:34:36 +0530

    Update dwh_ddl_bronze.sql

--- scripts/bronze/dwh_ddl_bronze.sql
+++ scripts/bronze/dwh_ddl_bronze.sql
@@ -1,13 +1,14 @@
 /*
-=============================================================================
+=========================================================================
 Script: Create Bronze Layer Tables
-=============================================================================
-Purpose: DDL script to create Bronze layer tables in the 'bronze' schema.
-Notes:
-  - Drops each bronze table if it exists, then recreates it.
-  - This is a DDL script for the bronze ETL layer.
-  - Run this script to redefine the DDL structure of 'bronze' Tables.
-=============================================================================
+=========================================================================
+Purpose: DDL script to create Bronze layer tables in the 'Bronze' schema.
+
+The script includes:
+1. Drops each bronze table if it exists, then recreates it.
+2. This is a DDL script for the bronze ETL layer.
+3. Run this script to redefine the DDL structure of 'Bronze' Tables.
+=========================================================================
 */
 
 IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL
